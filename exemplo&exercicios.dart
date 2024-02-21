53aca69d533991c3fb147ab9ac91d9763d60b531
JoaoGustavoDEVSenai <133894702+JoaoGustavoDEVSenai@users.noreply.github.com>
2024-02-21 12:04:55 -0300

    Add files via upload

---
+++ exemplo&exercicios.dart
@@ -0,0 +1,96 @@
+void main() {
+  /* EXEMPLO 1
+//String
+  String principal = 'main';
+  String NomeCombinado = 'José' + ' ' + 'Maria';
+  //Concatenação de String
+  String NomesCombinadosMais = 'José' ' ' 'Maria';
+  //String multiplas linhas
+  String EnderecoMultilinhas = '''Itapetininga, SP, 18200000, Cidade, Estado, CEP''';
+  
+  print(principal);
+  print(NomeCombinado);
+  print(NomesCombinadosMais);
+  print(EnderecoMultilinhas);
+  */
+
+  /* EXEMPLO 2
+  var nome = 'SESISENAI';
+  var idade = 1;
+  
+  var soma = 1 + 2;
+  var subtracao = 5 - 3;
+  var multiplicacao = 4 * 2;
+  var divisao = 10 / 2;
+  
+  print('Nome: $nome, Idade: $idade');
+  print('Soma: $soma, Subtração: $subtracao');
+  print('Multiplicação: $multiplicacao, Divisão: $divisao');
+  */
+
+  /* EXEMPLO 3
+  var nome = 'SESI';
+  var nome2 = 'SENAI';
+  
+  var nomeCompleto = nome + ' ' + nome2;
+  print('Nome completo: $nomeCompleto');
+  */
+
+  /* ESTRUTURA CONDICIONAL
+  var idade = 18;
+  if(idade >= 18){
+    print('Maior idade');
+  } else{
+    print('Menor idade');
+  }
+  */
+
+  /* OPERADOR TERNÁRIO
+  var idade = 18;
+  var msg = idade >= 18 ? 'Maior idade' : 'Menor idade';
+  print(msg);
+  */
+
+  /* SWITCH - CASE
+  var diaSemana = 'Segunda-feira';
+  
+  switch(diaSemana){
+    case 'Segunda-feira':
+      print('Hoje é Segunda-feira');
+      break;
+    case 'Terça-feira':
+      print('Hoje é Terça-feira');
+      break;
+    case 'Quarta-feira':
+      print('Hoje é Quarta-feira');
+      break;
+    default:
+      print('Dia da semana inválida');
+  }
+  */
+
+  /* ATV 1
+  int a = 20;
+  if (a % 5 == 0) {
+    print('O número $a ' ' é multiplo de 5');
+  } else {
+    print('O número $a não é multiplo de 5');
+  }
+  */
+
+  /* ATV 2
+  var num = 20;
+  var mensagem =
+      num % 2 == 0 ? 'O número $num é par.' : 'O número $num é ímpar';
+  print(mensagem);
+  */
+
+  /* ATV 3
+  var num = 10;
+  if (num % 2 == 0) {
+    print('O número $num é par');
+  } else {
+    print('O número $num é ímpar');
+  }
+  */
+}
